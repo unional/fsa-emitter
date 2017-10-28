fdca61ce443930bcb428b5e2f1ca26f970d55b37
Homa Wong <homawong@gmail.com>
2017-10-28 12:34:52 -0700

    BREAKING: change emit to emit `payload, meta, error`
    This allow consuming code to destructure `payload` so it is much easier to use.
    
    Consumer don't need to know action.type anyway as we have abstracted it out using the creator.type

--- src/createActionCreator.spec.ts
+++ src/createActionCreator.spec.ts
@@ -21,9 +21,20 @@ test('actionCreator with payload and meta creates FSA compliant action', t => {
   t.true(isFSA(withMeta(2, 'abc')))
 })
 
-test('creating with Error creates Error FSA', t => {
+test('Error payload creates Error FSA', t => {
   const withMeta = createActionCreator<Error, string>('withMeta')
   t.true(isError(withMeta(new Error('err'), 'abc')))
+})
+
+test('isError: true to create Error FSA', t => {
+  const customError = createActionCreator<{ x: number, err: Error }>('customError', true)
+  t.true(isError(customError({ x: 1, err: new Error() }, undefined)))
+})
+
+test(`isError predicate to create Error FSA`, t => {
+  const mayError = createActionCreator<{ x: number, err?: Error }>('mayError', (payload) => !!payload.err)
+  t.false(isError(mayError({ x: 1 }, undefined)))
+  t.true(isError(mayError({ x: 1, err: new Error() }, undefined)))
 })
 
 test('match should type guard an action', t => {
