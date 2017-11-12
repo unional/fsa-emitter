2a2603f5099d75276741fa0c57c19c2387e6ecfd
unional <homawong@gmail.com>
2017-11-12 00:47:50 -0800

    use setMaxListeners() in NodeJS

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,10 @@ export * from './createEvent'
 export * from './createEventAction'
 export * from './Emitter'
 export * from './errorEvent'
+
+import { isNode } from './environment'
+
+// We are using Emitter as store, just like redux.
+// Expect to have many listeners.
+if (isNode)
+  process.setMaxListeners(0)
