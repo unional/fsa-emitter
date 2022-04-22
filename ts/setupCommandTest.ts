769ee64d2e31a157d2d7fab9dc1b4a83141c6ccc
unional <homawong@gmail.com>
2022-04-21 21:36:12 -0700

    fix: update comments
    and keep comments

--- ts/setupCommandTest.ts
+++ ts/setupCommandTest.ts
@@ -2,7 +2,10 @@ import { Command } from './Command'
 import { Emitter } from './Emitter'
 import { TestEmitter } from './TestEmitter'
 
-export function setupCommandTest<Cmd extends Command, Args extends any[]>(Command: new (emitter: Emitter, ...args: Args) => Cmd, ...args: Args) {
+export function setupCommandTest<Cmd extends Command, Args extends any[]>(
+  Command: new (emitter: Emitter, ...args: Args) => Cmd,
+  ...args: Args
+) {
   const emitter = new TestEmitter()
   const command = new Command(emitter, ...args)
   return { emitter, command }
